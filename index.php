6725d97a7c68d27a989883ea739784f060102d6c
Ruben <33247852+RubenG06@users.noreply.github.com>
2018-01-14 12:27:41 +0100

    Reparado un pequeño fallo

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<pre><?php
-require_once('connection.php');
-$sql = 'SELECT * FROM gradoss';
-
-$statement = $pdo->prepare($sql);
-$statement->execute();
-$results = $statement->fetchAll();
-//var_dump($results);
-
-
-?>
+<pre>
 </pre>
 <!doctype html>
 <html class="no-js" lang="es">
